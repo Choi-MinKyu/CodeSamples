2c0a12617659406fa0a466005fbb2bd91e1c518e
Choi Min Kyu <ohiseaster@gmail.com>
2019-03-04 04:06:11 +0900

    RX ImageDownloader
    * URL Go 버튼 터치시 이미지 다운로드
    *  todo : Input, Output 별로 코드 리팩토링

---
+++ RXImageDown/RXImageDown/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  RXImageDown
+//
+//  Created by CHOI MIN KYU on 04/03/2019.
+//  Copyright © 2019 RX Samples. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Alamofire
+
+final class ViewController: UIViewController {
+	@IBOutlet weak var urlTextField: UITextField!
+	@IBOutlet weak var goButton: UIButton!
+	@IBOutlet weak var imageView: UIImageView!
+	
+	let disposeBag = DisposeBag()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		bind()
+	}
+}
+
+extension ViewController {
+	func bind() {
+		goButton.rx.tap.withLatestFrom(self.urlTextField.rx.text.orEmpty)
+			.map { text -> URL in
+				return try text.asURL()
+			}
+			.filter { (url: URL) -> Bool in
+				let imageExtension = ["jpg", "jpeg", "gif", "png"]
+				return imageExtension.contains(url.pathExtension)
+			}.debug()
+			.flatMap { (url: URL) -> Observable<String> in
+				return Observable<String>.create({ anyObserver -> Disposable in
+					let destination: DownloadRequest.DownloadFileDestination = { _, _ in
+						let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+						let file = directoryURL.appendingPathComponent(url.relativeString, isDirectory: false)
+						return (file, [.createIntermediateDirectories, .removePreviousFile])
+					}
+					
+					let download = Alamofire.download(url, to: destination)
+						.response { (response: DefaultDownloadResponse) in
+							if let data = response.destinationURL {
+								anyObserver.onNext(data.path)
+								anyObserver.onCompleted()
+							} else {
+								anyObserver.onError(RxError.unknown)
+							} }
+					return Disposables.create {
+						download.cancel()
+					}
+				})
+			}.map { (data: String) -> UIImage in
+				guard let image = UIImage(contentsOfFile: data)
+					else { throw RxError.noElements }
+				return image
+			}.bind(to: imageView.rx.image)
+			.disposed(by: disposeBag)
+	}
+}
+
---
+++ RXImageDown/RXImageDownTests/RXImageDownTests.swift
@@ -0,0 +1,34 @@
+//
+//  RXImageDownTests.swift
+//  RXImageDownTests
+//
+//  Created by CHOI MIN KYU on 04/03/2019.
+//  Copyright © 2019 RX Samples. All rights reserved.
+//
+
+import XCTest
+@testable import RXImageDown
+
+class RXImageDownTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
