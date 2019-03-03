af6ff1f9a71cb2989ca197de30fe227f650c3500
Choi Min Kyu <ohiseaster@gmail.com>
2019-03-04 03:30:32 +0900

    UI 작업
    * View를 움직이기 위한 BoxView Setting
    * 상, 하, 좌, 우 버튼 Setting

--- RXAnimation/RXAnimation/ViewController.swift
+++ RXAnimation/RXAnimation/ViewController.swift
@@ -8,8 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import RxSwift
+import RxCocoa
 
+enum Animation {
+	case left
+	case right
+	case down
+	case up
+}
+
+extension Animation {
+	func transform( _ transform: CGAffineTransform) -> CGAffineTransform {
+		switch self {
+		case .left:
+			return transform.translatedBy(x: -50, y: 0)
+		case .right:
+			return transform.translatedBy(x: 50, y: 0)
+		case .down:
+			return transform.translatedBy(x: 0, y: 50)
+		case .up:
+			return transform.translatedBy(x: 0, y: -50)
+		}
+	}
+}
+
+
+final class ViewController: UIViewController {
+	@IBOutlet weak var boxView: UIView!
+	@IBOutlet weak var upButton: UIButton!
+	@IBOutlet weak var leftButton: UIButton!
+	@IBOutlet weak var rightButton: UIButton!
+	@IBOutlet weak var downButton: UIButton!
+	
+	let disposeBag = DisposeBag()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
